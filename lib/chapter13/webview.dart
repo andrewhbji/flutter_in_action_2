0403f0c8306e4fb7537d977e5dd21cd75add772e
Andrea Ji <andrea.hb.ji@outlook.com>
2022-07-02 01:27:50 +0800

    更新环境设置能运行

--- lib/chapter13/webview.dart
+++ lib/chapter13/webview.dart
@@ -20,7 +20,7 @@ class _WebViewTestState extends State<WebViewTest> {
         onProgress: (progress) => print(progress),
         onPageStarted: (url) => print('start loading: $url'),
         onPageFinished: (url) => print('load finished:$url'),
-        onWebResourceError: (err)=>print(err.description),
+        onWebResourceError: (err) => print(err.description),
         javascriptChannels: <JavascriptChannel>{
           _toasterJavascriptChannel(context),
         },
@@ -33,7 +33,7 @@ class _WebViewTestState extends State<WebViewTest> {
       name: 'Toaster',
       onMessageReceived: (JavascriptMessage message) {
         // ignore: deprecated_member_use
-        Scaffold.of(context).showSnackBar(
+        ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text(message.message)),
         );
       },
